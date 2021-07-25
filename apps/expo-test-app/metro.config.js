24ada522930c5105aaf6033422d52e4078c938bf
Greg Westneat <greg.westneat@gmail.com>
2021-07-25 01:42:09 -0700

    feat: update metro config with SB defs

--- apps/expo-test-app/metro.config.js
+++ apps/expo-test-app/metro.config.js
@@ -1,5 +1,16 @@
 const { withNxMetro } = require('nx-react-native-expo');
 const { getDefaultConfig } = require('@expo/metro-config');
+// const { mergeConfig } = require('metro-config');
 
 const defaultConfig = getDefaultConfig(__dirname);
+
+
+defaultConfig.resolver.resolverMainFields.push('sbmodern', 'main');
+defaultConfig.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: false,
+    inlineRequires: false,
+  }
+})
+
 module.exports = withNxMetro(defaultConfig);
